e04576fdc7ba25d83d248e0d649afbb000571859
ashish.chodankar <ashish.chodankar@squareyards.co.in>
2023-07-07 15:28:26 +0530

    connect is used to connect compaonent to store;implements issue #5

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
+import { buyCake } from "../redux";
 
-function CakeContainer() {
+function CakeContainer(props) {
   return (
     <div>
-      <h2>Number of cakes</h2>
-      <button>Buy Cake</button>
+      <h2>Number of cakes-{props.numOfCakes}</h2>
+      <button onClick={props.buyCake}>Buy Cake</button>
     </div>
   );
 }
 
-export default CakeContainer;
+const mapStateToProps = (state) => {
+  //used for providing part of the state data from store to the components
+  return {
+    numOfCakes: state.numOfCakes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  //used for dispatching action to store
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer); //connect is used to connect store to the component,(connect takes two arguments mapStateToProps, mapDispatchToProps)
