a4b40c4f141768a35eea15c4b836b96938a8c58d
ashish.chodankar <ashish.chodankar@squareyards.co.in>
2023-07-18 18:24:14 +0530

    useDispatch hook used;implements issue #7

--- src/components/HooksCakeContainer.js
+++ src/components/HooksCakeContainer.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { buyCake } from "../redux/cake/cakeActions";
 
 const HooksCakeContainer = () => {
   const numOfCakes = useSelector((state) => state.numOfCakes);
+  const dispatch = useDispatch();
   return (
     <div>
       <h2>Num of cakes-{numOfCakes}</h2>
-      <button>Buy cake</button>
+      <button onClick={() => dispatch(buyCake())}>Buy cake</button>
     </div>
   );
 };
