06bb9c11f5dac90f157be36341cdda473253d454
ashish.chodankar <ashish.chodankar@squareyards.co.in>
2023-07-20 15:50:40 +0530

    mapStateToProps demo;implements issue #12

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ import { Provider } from "react-redux";
 import PizzaContainer from "./components/pizzaContainer";
 import NewCakeContainer from "./components/NewCakeContainer";
 import NewIceCreamContainer from "./components/NewIceCreamContainer";
+import ItemContainer from "./components/ItemContainer";
 
 function App() {
   return (
     // store connected to all components in a project
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
